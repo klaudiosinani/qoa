70d621d82e8f789e6bb34659200eaf82ebd63f3e
Klaus Sinani <klauscfhq@protonmail.com>
2019-02-05 21:52:13 +0200

    Added `menu` module

---
+++ src/menu.js
@@ -0,0 +1,25 @@
+'use strict';
+const events = require('events');
+const Prompt = require('./prompt');
+
+class Menu extends Prompt {
+  constructor(opts = {}) {
+    super(opts);
+    this._menu = opts.menu || [];
+    this._emitter = new events.EventEmitter();
+  }
+
+  _clearLines(n) {
+    for (let i = 0; i < n; i++) {
+      this._output.moveCursor(0, -1);
+      this._output.clearLine();
+      this._output.cursorTo(0);
+    }
+  }
+
+  _clearMenu() {
+    this._clearLines(this._menu.length);
+  }
+}
+
+module.exports = Menu;
