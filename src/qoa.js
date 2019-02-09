669560453badeed2d9d68e9dfb958c9e54ddfcd0
Davut KARA <davutkara.comtr@yandex.com>
2019-02-09 15:41:20 +0300

    feat: add clearScreen function to clear all outputs.

--- src/qoa.js
+++ src/qoa.js
@@ -62,6 +62,10 @@ class Qoa {
     return new Secure(this._buildConfig(x)).request();
   }
 
+  clearScreen(){
+    return process.stdout.write('\x1Bc');
+  }
+
   async prompt(questions) {
     const answers = {};
 
